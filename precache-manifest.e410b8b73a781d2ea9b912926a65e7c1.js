7e9026137b8047708c41b0a7b13f22364a3ef1c5
ArtemChubatyi <stgmdota@gmail.com>
2019-01-11 13:33:59 +0200

    Updates

---
+++ precache-manifest.e410b8b73a781d2ea9b912926a65e7c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "36ec40fe8038014cc89e289f0210545a",
+    "url": "/cryptoKittens/static/media/tag.36ec40fe.svg"
+  },
+  {
+    "revision": "235fe34a1d6465730fb1",
+    "url": "/cryptoKittens/static/js/runtime~main.235fe34a.js"
+  },
+  {
+    "revision": "355f09679fb3c26310f0",
+    "url": "/cryptoKittens/static/js/main.355f0967.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/cryptoKittens/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "355f09679fb3c26310f0",
+    "url": "/cryptoKittens/static/css/main.2cf1f0af.chunk.css"
+  },
+  {
+    "revision": "1d21e93b6c0e23f50cb780f237178296",
+    "url": "/cryptoKittens/index.html"
+  }
+];
