2fa01aecb987ee5c6ec2ba8dcd5e32752ea49071
ArtemChubatyi <stgmdota@gmail.com>
2019-01-16 15:31:20 +0200

    fixed #8

--- src/KittenStatus/KittenStatus.js
+++ src/KittenStatus/KittenStatus.js
@@ -4,9 +4,9 @@ import './KittenStatus.scss';
 
 export default function KittenStatus({ price }) {
     return (
-        <div className='kittenCard__status'>
-            <i className='icon' />
-            <p className='sale'>For sale</p>
-            <p className='price'>${price}</p>
+        <div className='kittenCard-status'>
+            <i className='kittenCard-status__icon' />
+            <p className='kittenCard-status__sale'>For sale</p>
+            <p className='kittenCard-status__price'>${price}</p>
         </div>)
 }
